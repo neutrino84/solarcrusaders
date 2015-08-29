2e2c900af49c9a7124df7b7f1ad395602738157b
Oliver Dyla <neutrino84@gmail.com>
2015-08-29 15:07:55 -0700

    added connect session and socket handling

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 var path = require('path'),
     express = require('express'),
+    session = require('express-session'),
     compression = require('compression'),
     favicon = require('serve-favicon'),
     bodyParser = require('body-parser'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io')(server),
+    ios = require('socket.io-express-session'),
     publicDir = path.resolve('public'),
     viewsDir = path.resolve('views'),
-    production = Boolean(process.env.PRODUCTION);
+    production = Boolean(process.env.PRODUCTION),
+    sess = session({
+      name: 'solar.sid',
+      secret: '4wWyR5Fq2vtKxq2mEjXkpEYM=4j-hz=X',
+      cookie: { path: '/', httpOnly: true, secure: false, maxAge: 86400000 },
+      saveUninitialized: true,
+      proxy: true,
+      resave: false,
+      rolling: true
+    });
 
-io.on('connection', function() {
-  console.log('connection established');
-});
-
+app.set('trust proxy', 1);
 app.set('views', viewsDir);
 app.set('view engine', 'jade');
 
@@ -21,12 +29,23 @@ app.use(favicon(publicDir + '/favicon.ico', { maxAge: 0 }));
 app.use(bodyParser.json());
 app.use(compression());
 app.use(express.static(publicDir));
+app.use(sess);
 
 app.get('/', function(req, res, next) {
+  var session = req.session;
+      session.hello = 'hi bobby';
   res.render('index', {
     title: 'Solar Crusaders',
     description: 'A multiplayer strategy game featuring 4X gameplay, sandbox universe, and simulated virtual economy.'
   });
+});
+
+io.use(ios(sess));
+io.on('connection', function(socket) {
+  var session = socket.handshake.session;
+
+  console.log('connection established');
+  console.log(session.hello);
 });
 
 server.listen(3000);
