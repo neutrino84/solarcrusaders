d3be6410f9dcb4ed8b0ba555e7db697aa90c2de9
Oliver Dyla <neutrino84@gmail.com>
2015-12-02 14:12:57 +0100

    increment version number

--- src/client/ui/panes/RightPane.js
+++ src/client/ui/panes/RightPane.js
@@ -71,7 +71,7 @@ function RightPane(game, settings) {
     })
 
   this.versionText = new Label(game,
-    'v1063', {
+    'v1070', {
       padding: [6, 8],
       text: {
         fontName: 'medium',
