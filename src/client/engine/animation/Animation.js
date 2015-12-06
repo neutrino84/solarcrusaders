f17af19cba92ab91621a8f792f526ed9473fc055
Oliver Dyla <neutrino84@gmail.com>
2015-12-06 12:51:10 +0100

    cleanup

--- src/client/engine/animation/Animation.js
+++ src/client/engine/animation/Animation.js
@@ -28,287 +28,285 @@ function Animation(game, parent, name, frameData, frames, frameRate, loop) {
 
   this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
 
-  //  Set-up some event listeners
+  ////  Set-up some event listeners
   // this.game.on('pause', this.onPause, this);
   // this.game.on('resume', this.onResume, this);
 
   EventEmitter.call(this);
 };
 
-Animation.prototype = {
+Animation.prototype = Object.create(EventEmitter.prototype);
+Animation.prototype.constructor = Animation;
 
-  play: function(frameRate, loop, killOnComplete) {
-    if(typeof frameRate === 'number') {
-      // if they set a new frame rate then use it, otherwise use the one set on creation
-      this.delay = 1000 / frameRate;
+Animation.prototype.play = function(frameRate, loop, killOnComplete) {
+  if(typeof frameRate === 'number') {
+    // if they set a new frame rate then use it, otherwise use the one set on creation
+    this.delay = 1000 / frameRate;
+  }
+
+  if(typeof loop === 'boolean') {
+    // if they set a new loop value then use it, otherwise use the one set on creation
+    this.loop = loop;
+  }
+
+  if(killOnComplete !== undefined) {
+    // remove the parent sprite once the animation has finished?
+    this.killOnComplete = killOnComplete;
+  }
+
+  this.isPlaying = true;
+  this.isFinished = false;
+  this.paused = false;
+  this.loopCount = 0;
+
+  this._timeLastFrame = this.game.clock.time;
+  this._timeNextFrame = this.game.clock.time + this.delay;
+
+  this._frameIndex = 0;
+  this.updateCurrentFrame(true);
+
+  this.emit('start', this.parent, this);
+
+  this.parent.emit('animationStart', this);
+  this.parent.animations.currentAnim = this;
+  this.parent.animations.currentFrame = this.currentFrame;
+
+  return this;
+};
+
+Animation.prototype.restart = function() {
+  this.isPlaying = true;
+  this.isFinished = false;
+  this.paused = false;
+  this.loopCount = 0;
+
+  this._timeLastFrame = this.game.clock.time;
+  this._timeNextFrame = this.game.clock.time + this.delay;
+
+  this._frameIndex = 0;
+
+  this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
+
+  this.parent.setFrame(this.currentFrame);
+
+  this.emit('start', this.parent, this);
+
+  this.parent.emit('animationStart', this);
+  this.parent.animations.currentAnim = this;
+  this.parent.animations.currentFrame = this.currentFrame;
+};
+
+Animation.prototype.setFrame = function(frameId, useLocalFrameIndex) {
+  var frameIndex;
+
+  if(useLocalFrameIndex === undefined) {
+    useLocalFrameIndex = false;
+  }
+
+  // find the index to the desired frame.
+  if(typeof frameId === 'string') {
+    for (var i = 0; i < this._frames.length; i++) {
+      if(this._frameData.getFrame(this._frames[i]).name === frameId) {
+        frameIndex = i;
+      }
     }
-
-    if(typeof loop === 'boolean') {
-      // if they set a new loop value then use it, otherwise use the one set on creation
-      this.loop = loop;
-    }
-
-    if(killOnComplete !== undefined) {
-      // remove the parent sprite once the animation has finished?
-      this.killOnComplete = killOnComplete;
-    }
-
-    this.isPlaying = true;
-    this.isFinished = false;
-    this.paused = false;
-    this.loopCount = 0;
-
-    this._timeLastFrame = this.game.clock.time;
-    this._timeNextFrame = this.game.clock.time + this.delay;
-
-    this._frameIndex = 0;
-    this.updateCurrentFrame(true);
-
-    this.emit('start', this.parent, this);
-
-    this.parent.emit('animationStart', this);
-    this.parent.animations.currentAnim = this;
-    this.parent.animations.currentFrame = this.currentFrame;
-
-    return this;
-  },
-
-  restart: function() {
-    this.isPlaying = true;
-    this.isFinished = false;
-    this.paused = false;
-    this.loopCount = 0;
-
-    this._timeLastFrame = this.game.clock.time;
-    this._timeNextFrame = this.game.clock.time + this.delay;
-
-    this._frameIndex = 0;
-
-    this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
-
-    this.parent.setFrame(this.currentFrame);
-
-    this.emit('start', this.parent, this);
-
-    this.parent.emit('animationStart', this);
-    this.parent.animations.currentAnim = this;
-    this.parent.animations.currentFrame = this.currentFrame;
-  },
-
-  setFrame: function(frameId, useLocalFrameIndex) {
-    var frameIndex;
-
-    if(useLocalFrameIndex === undefined) {
-      useLocalFrameIndex = false;
-    }
-
-    // find the index to the desired frame.
-    if(typeof frameId === 'string') {
+  } else if(typeof frameId === 'number') {
+    if(useLocalFrameIndex) {
+      frameIndex = frameId;
+    } else {
       for (var i = 0; i < this._frames.length; i++) {
-        if(this._frameData.getFrame(this._frames[i]).name === frameId) {
+        if(this._frames[i] === frameIndex) {
           frameIndex = i;
         }
       }
-    } else if(typeof frameId === 'number') {
-      if(useLocalFrameIndex) {
-        frameIndex = frameId;
-      } else {
-        for (var i = 0; i < this._frames.length; i++) {
-          if(this._frames[i] === frameIndex) {
-            frameIndex = i;
-          }
-        }
-      }
     }
+  }
 
-    if(frameIndex) {
-      // set the current frame index to the found index.
-      // subtract 1 so that it animates to the desired frame on update.
-      this._frameIndex = frameIndex - 1;
+  if(frameIndex) {
+    // set the current frame index to the found index.
+    // subtract 1 so that it animates to the desired frame on update.
+    this._frameIndex = frameIndex - 1;
 
-      // make the animation update at next update
-      this._timeNextFrame = this.game.clock.time;
+    // make the animation update at next update
+    this._timeNextFrame = this.game.clock.time;
 
-      this.update();
-    }
-  },
-
-  stop: function(resetFrame, dispatchComplete) {
-    if(resetFrame === undefined) { resetFrame = false; }
-    if(dispatchComplete === undefined) { dispatchComplete = false; }
-
-    this.isPlaying = false;
-    this.isFinished = true;
-    this.paused = false;
-
-    if(resetFrame) {
-      this.currentFrame = this._frameData.getFrame(this._frames[0]);
-      this.parent.setFrame(this.currentFrame);
-    }
-
-    if(dispatchComplete) {
-      this.parent.emit('animationComplete', this);
-      this.emit('complete', this.parent, this);
-    }
-  },
-
-  onPause: function() {
-    if(this.isPlaying) {
-      this._frameDiff = this._timeNextFrame - this.game.clock.time;
-    }
-  },
-
-  onResume: function() {
-    if(this.isPlaying) {
-      this._timeNextFrame = this.game.clock.time + this._frameDiff;
-    }
-  },
-
-  update: function() {
-    if(this.isPaused) { return false; }
-
-    if(this.isPlaying && this.game.clock.time >= this._timeNextFrame) {
-      this._frameSkip = 1;
-
-      //  Lagging?
-      this._frameDiff = this.game.clock.time - this._timeNextFrame;
-      this._timeLastFrame = this.game.clock.time;
-
-      if(this._frameDiff > this.delay) {
-        //  We need to skip a frame, work out how many
-        this._frameSkip = Math.floor(this._frameDiff / this.delay);
-        this._frameDiff -= (this._frameSkip * this.delay);
-      }
-
-      //  And what's left now?
-      this._timeNextFrame = this.game.clock.time + (this.delay - this._frameDiff);
-      this._frameIndex += this._frameSkip;
-
-      if(this._frameIndex >= this._frames.length) {
-        if(this.loop) {
-          // Update current state before event callback
-          this._frameIndex %= this._frames.length;
-          this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
-
-          //  Instead of calling updateCurrentFrame we do it here instead
-          if(this.currentFrame) {
-            this.parent.setFrame(this.currentFrame);
-          }
-
-          this.loopCount++;
-
-          // dispatch events
-          // this.parent.emit('animationLoop', this);
-          // this.emit('loop', this.parent, this);
-
-          return true;
-        } else {
-          this.complete();
-          return false;
-        }
-      } else {
-        return this.updateCurrentFrame();
-      }
-    }
-
-    return false;
-  },
-
-  updateCurrentFrame: function(fromPlay) {
-    if(fromPlay === undefined) { fromPlay = false; }
-
-    if(!this._frameData) {
-      // the animation is already destroyed, probably from a callback
-      return false;
-    }
-
-    // previous index
-    var idx = this.currentFrame.index;
-
-    this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
-
-    if(this.currentFrame && (fromPlay || (!fromPlay && idx !== this.currentFrame.index))) {
-      this.parent.setFrame(this.currentFrame);
-    }
-
-    return true;
-  },
-
-  next: function(quantity) {
-    if(quantity === undefined) { quantity = 1; }
-
-    var frame = this._frameIndex + quantity;
-    if(frame >= this._frames.length) {
-      if(this.loop) {
-        frame %= this._frames.length;
-      } else {
-        frame = this._frames.length - 1;
-      }
-    }
-
-    if(frame !== this._frameIndex) {
-      this._frameIndex = frame;
-      this.updateCurrentFrame();
-    }
-  },
-
-  previous: function(quantity) {
-    if(quantity === undefined) { quantity = 1; }
-
-    var frame = this._frameIndex - quantity;
-    if(frame < 0) {
-      if(this.loop) {
-        frame = this._frames.length + frame;
-      } else {
-        frame++;
-      }
-    }
-
-    if(frame !== this._frameIndex) {
-      this._frameIndex = frame;
-      this.updateCurrentFrame();
-    }
-  },
-
-  updateFrameData: function(frameData) {
-    this._frameData = frameData;
-    this.currentFrame = this._frameData ? this._frameData.getFrame(this._frames[this._frameIndex % this._frames.length]) : null;
-  },
-
-  destroy: function() {
-    if(!this._frameData) { return; }
-
-    // this.game.removeListener('pause', this.onPause, this);
-    // this.game.removeListener('resume', this.onResume, this);
-
-    this.game = null;
-    this.parent = null;
-    this._frames = null;
-    this._frameData = null;
-    this.currentFrame = null;
-    this.isPlaying = false;
-
-    this.removeAllListeners();
-  },
-
-  complete: function() {
-    this._frameIndex = this._frames.length - 1;
-    this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
-
-    this.isPlaying = false;
-    this.isFinished = true;
-    this.paused = false;
-
-    this.parent.on('animationComplete', this);
-    this.on('complete', this.parent, this);
-
-    if(this.killOnComplete) {
-      this.parent.kill();
-    }
+    this.update();
   }
 };
 
-Animation.prototype.constructor = Animation;
+Animation.prototype.stop = function(resetFrame, dispatchComplete) {
+  if(resetFrame === undefined) { resetFrame = false; }
+  if(dispatchComplete === undefined) { dispatchComplete = false; }
+
+  this.isPlaying = false;
+  this.isFinished = true;
+  this.paused = false;
+
+  if(resetFrame) {
+    this.currentFrame = this._frameData.getFrame(this._frames[0]);
+    this.parent.setFrame(this.currentFrame);
+  }
+
+  if(dispatchComplete) {
+    this.parent.emit('animationComplete', this);
+    this.emit('complete', this.parent, this);
+  }
+};
+
+Animation.prototype.onPause = function() {
+  if(this.isPlaying) {
+    this._frameDiff = this._timeNextFrame - this.game.clock.time;
+  }
+};
+
+Animation.prototype.onResume = function() {
+  if(this.isPlaying) {
+    this._timeNextFrame = this.game.clock.time + this._frameDiff;
+  }
+};
+
+Animation.prototype.update = function() {
+  if(this.isPaused) { return false; }
+
+  if(this.isPlaying && this.game.clock.time >= this._timeNextFrame) {
+    this._frameSkip = 1;
+
+    //  Lagging?
+    this._frameDiff = this.game.clock.time - this._timeNextFrame;
+    this._timeLastFrame = this.game.clock.time;
+
+    if(this._frameDiff > this.delay) {
+      //  We need to skip a frame, work out how many
+      this._frameSkip = Math.floor(this._frameDiff / this.delay);
+      this._frameDiff -= (this._frameSkip * this.delay);
+    }
+
+    //  And what's left now?
+    this._timeNextFrame = this.game.clock.time + (this.delay - this._frameDiff);
+    this._frameIndex += this._frameSkip;
+
+    if(this._frameIndex >= this._frames.length) {
+      if(this.loop) {
+        // Update current state before event callback
+        this._frameIndex %= this._frames.length;
+        this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
+
+        //  Instead of calling updateCurrentFrame we do it here instead
+        if(this.currentFrame) {
+          this.parent.setFrame(this.currentFrame);
+        }
+
+        this.loopCount++;
+
+        // dispatch events
+        // this.parent.emit('animationLoop', this);
+        // this.emit('loop', this.parent, this);
+
+        return true;
+      } else {
+        this.complete();
+        return false;
+      }
+    } else {
+      return this.updateCurrentFrame();
+    }
+  }
+
+  return false;
+};
+
+Animation.prototype.updateCurrentFrame = function(fromPlay) {
+  if(fromPlay === undefined) { fromPlay = false; }
+
+  if(!this._frameData) {
+    // the animation is already destroyed, probably from a callback
+    return false;
+  }
+
+  // previous index
+  var idx = this.currentFrame.index;
+
+  this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
+
+  if(this.currentFrame && (fromPlay || (!fromPlay && idx !== this.currentFrame.index))) {
+    this.parent.setFrame(this.currentFrame);
+  }
+
+  return true;
+};
+
+Animation.prototype.next = function(quantity) {
+  if(quantity === undefined) { quantity = 1; }
+
+  var frame = this._frameIndex + quantity;
+  if(frame >= this._frames.length) {
+    if(this.loop) {
+      frame %= this._frames.length;
+    } else {
+      frame = this._frames.length - 1;
+    }
+  }
+
+  if(frame !== this._frameIndex) {
+    this._frameIndex = frame;
+    this.updateCurrentFrame();
+  }
+};
+
+Animation.prototype.previous = function(quantity) {
+  if(quantity === undefined) { quantity = 1; }
+
+  var frame = this._frameIndex - quantity;
+  if(frame < 0) {
+    if(this.loop) {
+      frame = this._frames.length + frame;
+    } else {
+      frame++;
+    }
+  }
+
+  if(frame !== this._frameIndex) {
+    this._frameIndex = frame;
+    this.updateCurrentFrame();
+  }
+};
+
+Animation.prototype.updateFrameData = function(frameData) {
+  this._frameData = frameData;
+  this.currentFrame = this._frameData ? this._frameData.getFrame(this._frames[this._frameIndex % this._frames.length]) : null;
+};
+
+Animation.prototype.destroy = function() {
+  if(!this._frameData) { return; }
+
+  // this.game.removeListener('pause', this.onPause, this);
+  // this.game.removeListener('resume', this.onResume, this);
+
+  this.game = null;
+  this.parent = null;
+  this._frames = null;
+  this._frameData = null;
+  this.currentFrame = null;
+  this.isPlaying = false;
+
+  this.removeAllListeners();
+};
+
+Animation.prototype.complete = function() {
+  this._frameIndex = this._frames.length - 1;
+  this.currentFrame = this._frameData.getFrame(this._frames[this._frameIndex]);
+
+  this.isPlaying = false;
+  this.isFinished = true;
+  this.paused = false;
+
+  this.parent.on('animationComplete', this);
+  this.on('complete', this.parent, this);
+
+  if(this.killOnComplete) {
+    this.parent.kill();
+  }
+};
 
 Object.defineProperty(Animation.prototype, 'paused', {
   get: function() {
