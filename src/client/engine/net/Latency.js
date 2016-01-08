42ac94a62efa41e36cd58238e6222ed7de5d8da8
Oliver Dyla <neutrino84@gmail.com>
2016-01-07 19:02:36 -0800

    added average to latency computation

--- src/client/engine/net/Latency.js
+++ src/client/engine/net/Latency.js
@@ -7,14 +7,31 @@ function Latency(game) {
   this.endTime = 0;
   this.rtt = 0;
 
+  this._history = [];
+
   this.socket.on('ping', this.ping.bind(this));
 };
+
+Latency.HISTORY_SIZE = 10;
 
 Latency.prototype.constructor = Latency;
 
 Latency.prototype.ping = function(data) {
-  this.rtt = data.rtt;
   this.socket.emit('pong');
+  this._compute(data);
+};
+
+Latency.prototype._compute = function(data) {
+  var average = 0,
+      history = this._history;
+      history.push(data.rtt);
+  if(history.length > Latency.HISTORY_SIZE) {
+    history.shift();
+  }
+  for(var rtt in history) {
+    average += history[rtt];
+  }
+  this.rtt = global.Math.round(average / history.length);
 };
 
 module.exports = Latency;
