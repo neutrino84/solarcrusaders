7e318389493dbaf827ead8cd8e903c76d8cdac9e
Oliver Dyla <neutrino84@gmail.com>
2016-01-20 14:37:04 -0800

    added support for tab buttons and improved styling

--- src/client/ui/panes/ContentPane.js
+++ src/client/ui/panes/ContentPane.js
@@ -23,22 +23,22 @@ function ContentPane(game, string, settings) {
       radius: 0.0
     },
     title: {
-      padding: [0],
+      padding: [1, 1, 0, 1],
       layout: {
         type: 'border',
-        ax: 0,
+        ax: 1,
         ay: 0
       },
       bg: {
         fillAlpha: 1.0,
-        color: 0x666666,
+        color: 0x585858,
         radius: 0.0,
         borderSize: 0.0,
         blendMode: engine.BlendMode.MULTIPLY
       }
     },
     content: {
-      padding: [3, 3],
+      padding: [1],
       bg: {
         fillAlpha: 0.8,
         color: 0x000000,
@@ -47,26 +47,38 @@ function ContentPane(game, string, settings) {
         blendMode: engine.BlendMode.MULTIPLY
       }
     },
+    tabs: {
+      padding: [0],
+      layout: {
+        ax: Layout.LEFT,
+        ay: Layout.TOP,
+        direction: Layout.HORIZONTAL,
+        gap: 0
+      },
+      bg: {
+        fillAlpha: 0.0,
+        radius: 0.0,
+        borderSize: 0.0
+      }
+    },
     button: {
       padding: [0],
       bg: {
-        highlight: 0x002040,
-        color: 0x3868b8,
-        fillAlpha: 0.5,
-        blendMode: engine.BlendMode.ADD,
-        borderSize: 0,
-        radius: 0
+        fillAlpha: 1.0,
+        borderSize: 0.0,
+        radius: 0.0
       },
       label: {
-        padding: [6, 8],
+        padding: [5, 8, 6, 8],
         border: [0],
         text: {
           fontName: 'small'
         },
         bg: {
-          borderSize: 0,
+          highlight: false,
+          borderSize: 0.0,
           fillAlpha: 0.0,
-          radius: 0
+          radius: 0.0
         }
       }
     }
@@ -84,6 +96,7 @@ function ContentPane(game, string, settings) {
   this.title = new Pane(game, this.settings.title);
   this.content = new Pane(game, this.settings.content);
   this.button = new Button(game, string, this.settings.button);
+  this.tabs = new Pane(game, this.settings.tabs);
   
   this.label = this.button.label;
 
@@ -91,11 +104,17 @@ function ContentPane(game, string, settings) {
 
   this.addPanel(Layout.CENTER, this.content);
   this.addPanel(Layout.TOP, this.title);
+
   this.title.addPanel(Layout.LEFT, this.button);
+  this.title.addPanel(Layout.CENTER, this.tabs);
 };
 
 ContentPane.prototype = Object.create(Panel.prototype);
 ContentPane.prototype.constructor = ContentPane;
+
+ContentPane.prototype.addTab = function(constraint, panel) {
+  this.tabs.addPanel(constraint, panel);
+};
 
 ContentPane.prototype.addContent = function(constraint, panel) {
   this.content.addPanel(constraint, panel);
