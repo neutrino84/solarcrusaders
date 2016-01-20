222d5291e1c23e844b02ab12ea1fe60b3b621b15
Oliver Dyla <neutrino84@gmail.com>
2016-01-20 14:47:09 -0800

    added new layout type percent based

---
+++ src/client/ui/layouts/PercentLayout.js
@@ -0,0 +1,93 @@
+
+var Layout = require('../Layout');
+
+function PercentLayout(dir, gap, stretch) {
+  this.direction = dir || Layout.HORIZONTAL;
+  this.gap = gap || 0;
+  this.stretch = stretch || true;
+};
+
+PercentLayout.prototype.calcPreferredSize = function(target) {
+  var d, max = 0,
+      length = target.panels.length,
+      as = this.gap * (length === 0 ? 0 : length-1);
+
+  for(var i=0; i<length; i++) {
+    d = target.panels[i].getPreferredSize();
+    if(this.direction == Layout.HORIZONTAL) {
+      if(d.height > max) {
+        max = d.height;
+      }
+      as += d.width;
+    } else {
+      if(d.width > max) {
+        max = d.width;
+      }
+      as += d.height;
+    }
+  }
+  return this.direction == Layout.HORIZONTAL ? { width: as, height: max } : { width: max, height: as };
+};
+
+PercentLayout.prototype.doLayout = function(target) {
+  var child, constraint, useps,
+      right = target.right,
+      top = target.top,
+      bottom = target.bottom,
+      left = target.left,
+      length = target.panels.length,
+      rs = -this.gap * (length === 0 ? 0 : length - 1),
+      loc, ns, yy, hh, ph, xx, ww, pw;
+
+  if(this.direction == Layout.HORIZONTAL) {
+    rs += target.size.width-left-right;
+    loc = left;
+  } else {
+    rs += target.size.height-top-bottom;
+    loc = top;
+  }
+
+  for(var i=0; i<length; i++) {
+    child = target.panels[i];
+    constraint = child.constraint;
+    useps = (constraint == Layout.USE_PS_SIZE);
+
+    if(this.direction == Layout.HORIZONTAL) {
+      ns = ((length - 1) == i) ? target.size.width - right - loc : (
+        useps ? child.getPreferredSize().width : ~~((rs * constraint) / 100)
+      );
+      
+      yy = top;
+      hh = target.size.height - top - bottom;
+      
+      if(this.stretch === false) {
+        ph = hh;
+        hh = child.getPreferredSize().height;
+        yy = top + ~~((ph - hh) / 2);
+      }
+
+      child.setLocation(loc, yy);
+      child.setSize(ns, hh);
+    } else {
+      ns = ((length - 1) == i) ? target.size.height - bottom - loc : (
+        useps ? child.getPreferredSize().height : ~~((rs * constraint) / 100)
+      );
+      
+      xx = left;
+      ww = target.size.width - left - right;
+      
+      if(this.stretch === false) {
+        pw = ww;
+        ww = child.getPreferredSize().width;
+        xx = left + ~~((pw - ww)/2);
+      }
+
+      child.setLocation(xx, loc);
+      child.setSize(ww, ns);
+    }
+
+    loc += (ns + this.gap);
+  }
+};
+
+module.exports = PercentLayout;
