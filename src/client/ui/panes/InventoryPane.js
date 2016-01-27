d09f281905b6673e3da992320f2e825188e6553e
Oliver Dyla <neutrino84@gmail.com>
2016-01-27 05:02:49 +0100

    added empty list items to inventory and subsystems

--- src/client/ui/panes/InventoryPane.js
+++ src/client/ui/panes/InventoryPane.js
@@ -1,6 +1,7 @@
 
 var engine = require('engine'),
     Layout = require('../Layout'),
+    Pane = require('../components/Pane'),
     ContentPane = require('./ContentPane'),
     Class = engine.Class;
 
@@ -8,8 +9,14 @@ function InventoryPane(game, settings) {
   ContentPane.call(this, game, 'items',
     Class.mixin(settings, {
       padding: [1],
-      width: 128,
-      height: 72,
+      content: {
+        padding: [2],
+        layout: {
+          type: 'list',
+          gap: [2, 2],
+          columns: 4
+        }
+      },
       button: {
         label: {
           padding: [2]
@@ -17,9 +24,31 @@ function InventoryPane(game, settings) {
       }
     })
   );
+
+  // create
+  this.create();
 };
 
 InventoryPane.prototype = Object.create(ContentPane.prototype);
 InventoryPane.prototype.constructor = InventoryPane;
+
+InventoryPane.prototype.create = function() {
+  var len = 8;
+  for(var i=0; i<len; i++) {
+    this.addContent(Layout.NONE, this.createEmptyPane());
+  }
+};
+
+InventoryPane.prototype.createEmptyPane = function() {
+  return new Pane(this.game, {
+    width: 27,
+    height: 27,
+    bg: {
+      fillAlpha: 0.75,
+      color: 0x000000,
+      borderSize: 0.0
+    }
+  });
+};
 
 module.exports = InventoryPane;
--- src/client/ui/panes/SubsystemPane.js
+++ src/client/ui/panes/SubsystemPane.js
@@ -1,16 +1,22 @@
 
 var engine = require('engine'),
     Layout = require('../Layout'),
+    Pane = require('../components/Pane'),
     ContentPane = require('./ContentPane'),
     Class = engine.Class;
 
 function SubsystemPane(game, settings) {
-  ContentPane.call(this, game, 'subsystems',
+  ContentPane.call(this, game, 'sub',
     Class.mixin(settings, {
       padding: [1],
-      border: [0],
-      width: 96,
-      height: 32,
+      content: {
+        padding: [2],
+        layout: {
+          type: 'list',
+          gap: [2, 2],
+          columns: 1
+        }
+      },
       button: {
         label: {
           padding: [2]
@@ -18,9 +24,31 @@ function SubsystemPane(game, settings) {
       }
     })
   );
+
+  // create
+  this.create();
 };
 
 SubsystemPane.prototype = Object.create(ContentPane.prototype);
 SubsystemPane.prototype.constructor = SubsystemPane;
+
+SubsystemPane.prototype.create = function() {
+  var len = 2;
+  for(var i=0; i<len; i++) {
+    this.addContent(Layout.NONE, this.createEmptyPane());
+  }
+};
+
+SubsystemPane.prototype.createEmptyPane = function() {
+  return new Pane(this.game, {
+    width: 27,
+    height: 27,
+    bg: {
+      fillAlpha: 0.75,
+      color: 0x000000,
+      borderSize: 0.0
+    }
+  });
+};
 
 module.exports = SubsystemPane;
