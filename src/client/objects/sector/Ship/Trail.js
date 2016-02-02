667802a105684a2b20191aaae2bc8d9a3159c0db
Oliver Dyla <neutrino84@gmail.com>
2016-02-02 11:29:43 +0100

    updated ship engine trails implementation

--- src/client/objects/sector/Ship/Trail.js
+++ src/client/objects/sector/Ship/Trail.js
@@ -6,6 +6,8 @@ function Trail(parent) {
   this.game = parent.game;
   this.trajectoryGroup = parent.manager.trajectoryGroup;
 
+  this._tempPoint = new engine.Point();
+
   this.numOfPoints = 8;
 };
 
@@ -13,7 +15,7 @@ Trail.prototype.constructor = Trail;
 
 Trail.prototype.create = function() {
   var points = this.points = [],
-       center, parent = this.parent,
+      center, parent = this.parent,
       config = parent.config.engine.trail;
   if(config) {
     center = game.world.worldTransform.applyInverse(parent.worldTransform.apply(config.position));
@@ -28,7 +30,18 @@ Trail.prototype.create = function() {
     this.trail.alpha = 0.75;
 
     this.trajectoryGroup.add(this.trail);
+
+    parent.on('enterBounds', this.enterBounds, this);
+    parent.on('exitBounds', this.exitBounds, this);
   }
+};
+
+Trail.prototype.enterBounds = function() {
+  this.trajectoryGroup.add(this.trail);
+};
+
+Trail.prototype.exitBounds = function() {
+  this.trajectoryGroup.remove(this.trail);
 };
 
 Trail.prototype.update = function() {
@@ -37,22 +50,29 @@ Trail.prototype.update = function() {
       points = this.points,
       len = this.numOfPoints,
       config = parent.config.engine.trail,
-      worldTransform = this.game.world.worldTransform;
+      worldTransform = this.game.world.worldTransform,
+      vector;
   if(config) {
-    center = worldTransform.applyInverse(parent.worldTransform.apply(config.position))
+    center = worldTransform.applyInverse(parent.worldTransform.apply(config.position, this._tempPoint), this._tempPoint);
     for(var i=0; i<len; i++) {
-      if(i == len-1) {
-        pos = center;
+      if(i==len-1) {
+        points[i].set(center.x, center.y);
+      } else if(i==len-2) {
+        vector = parent.movement.vector.multiply(3.0, 3.0);
+        points[i].set(center.x-vector.x, center.y-vector.y);
       } else {
         pos = points[i+1];
+        points[i].interpolate(pos, (i*2) * 0.01, points[i]);
       }
-      points[i].interpolate(pos, (i*2) * 0.015, points[i]);
     }
   }
 };
 
 Trail.prototype.destroy = function() {
   this.trajectoryGroup.remove(this.trail);
+
+  this.parent.removeListener('enterBounds', this.enterBounds);
+  this.parent.removeListener('exitBounds', this.exitBounds);
 
   this.trail = this.points = this.parent =
     this.game = this.trajectoryGroup = undefined;
