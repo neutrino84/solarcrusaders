0aec2c1beed2ff11b5ae2a2a271adda74394ceae
Oliver Dyla <neutrino84@gmail.com>
2016-02-10 08:58:19 +0100

    updated explosion emitter properties

--- src/client/objects/sector/emitters/ExplosionEmitter.js
+++ src/client/objects/sector/emitters/ExplosionEmitter.js
@@ -7,7 +7,7 @@ function ExplosionEmitter(game) {
   this.minRotation = -30;
   this.maxRotation = 30;
 
-  this.setScale(0.25, 0.8, 0.25, 0.8, 2000);
+  this.setScale(0.5, 1.2, 0.5, 1.2, 2000);
   this.setAlpha(1.0, 0.0, 3000);
   this.setTint(0xFF6666, 0x000000, 750);
 
