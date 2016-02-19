80b75278090b630b39a936ef9385f23e6ac60475
Oliver Dyla <neutrino84@gmail.com>
2016-02-19 15:34:50 +0100

    removed logout button

--- src/client/ui/panes/LeftPane.js
+++ src/client/ui/panes/LeftPane.js
@@ -7,7 +7,7 @@ var xhr = require('xhr'),
 
 function LeftPane(game, settings) {
   Pane.call(this, game, {
-    padding: [6],
+    padding: [20], //[6],
     layout: {
       gap: 6
     },
@@ -18,34 +18,9 @@ function LeftPane(game, settings) {
       radius: 1
     }
   });
-
-  this.icon1 = new ButtonIcon(game, 'texture-atlas', { icon: { frame: 'icon-x01.png' }});
-  this.icon1.on('inputUp', this._logout, this);
-
-  this.addPanel(Layout.NONE, this.icon1);
 };
 
 LeftPane.prototype = Object.create(Pane.prototype);
 LeftPane.prototype.constructor = LeftPane;
-
-LeftPane.prototype._logout = function() {
-  var self = this,
-      header = {
-        method: 'get',
-        uri: '/logout',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-  xhr(header, function(err, resp, body) {
-    var response = JSON.parse(body),
-        user = response.user,
-        error = err || response.error;
-    if(error) {
-      self.game.emit('gui/alert', 'an unknown error has occurred');
-    }
-    self.game.emit('gui/logout');
-  });
-};
 
 module.exports = LeftPane;
