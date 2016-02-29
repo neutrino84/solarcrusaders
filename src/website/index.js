7837837921dd514cf8baacb0187a3c8cde7c7803
Oliver Dyla <neutrino84@gmail.com>
2016-02-29 18:13:55 +0100

    uploaded website src and build

---
+++ src/website/index.js
@@ -0,0 +1,31 @@
+var jquery = require('jquery'),
+    fullPage = require('fullpage'),
+    slick = require('slick');
+
+jquery(document).ready(function() {
+  // fullpage
+  jquery('#window').fullpage({
+  	// loopBottom: true,
+    animateAnchor: false,
+    menu: '#navigation',
+    anchors: ['home', 'about', 'races'],
+    onLeave: function(index, nextIndex) {
+      if(nextIndex === 1) {
+      	jquery('#logo').removeClass('show');
+      } else {
+      	jquery('#logo').addClass('show');
+      }
+    }
+  });
+
+  // slick
+  jquery('#abt .screenshot').slick({
+    dots: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    easing: 'easeInOutCubic',
+   	slide: '.img',
+    arrows: false
+  });
+});
