79ed9d38516d6a4817dc8ea9969dc8bff013fd77
Oliver Dyla <neutrino84@gmail.com>
2016-03-03 17:10:14 +0100

    improved selection indicator look and feel

--- src/client/objects/sector/misc/Indicator.js
+++ src/client/objects/sector/misc/Indicator.js
@@ -1,35 +1,56 @@
 
 var engine = require('engine');
 
-function Indicator(game) {
-  engine.Sprite.call(this, game, 'texture-atlas', 'icon-target.png');
+function Indicator(game, parent) {
+  engine.Group.call(this, game, parent);
 
-  this.tint = 0x336699;
-  this.blendMode = engine.BlendMode.ADD;
-  this.pivot.set(20, 20);
   this.renderable = false;
 
-  this.tween = this.game.tweens.create(this);
-  this.tween.to({ alpha: 0.0, rotation: 2*global.Math.PI/3 }, 500, engine.Easing.Quadratic.Out);
-  this.tween.on('complete', function(tween) {
-    tween.target.renderable = false;
-  });
+  this.sprite1 = new engine.Sprite(game, 'texture-atlas', 'icon-target.png');
+  this.sprite1.tint = 0x55aaff;
+  this.sprite1.pivot.set(20, 20);
+  this.sprite1.blendMode = engine.BlendMode.ADD;
+
+  this.sprite2 = new engine.Sprite(game, 'texture-atlas', 'icon-target.png');
+  this.sprite2.tint = 0x55aaff;
+  this.sprite2.pivot.set(20, 20);
+  this.sprite2.scale.set(0.5, 0.5);
+  this.sprite2.blendMode = engine.BlendMode.ADD;
+
+  this.tween1 = this.game.tweens.create(this.sprite1);
+  this.tween1.to({ alpha: 0.0, rotation: 2 * global.Math.PI / 3 }, 500, engine.Easing.Quadratic.Out);
+  
+  this.tween2 = this.game.tweens.create(this.sprite2);
+  this.tween2.to({ alpha: 0.0, rotation: -2 * global.Math.PI / 6 }, 500, engine.Easing.Quadratic.Out);
+  this.tween2.on('complete', function(tween) {
+    this.renderable = false;
+  }, this);
+
+  this.add(this.sprite1);
+  this.add(this.sprite2);
 };
 
-Indicator.prototype = Object.create(engine.Sprite.prototype);
+Indicator.prototype = Object.create(engine.Group.prototype);
 Indicator.prototype.constructor = Indicator;
 
 Indicator.prototype.show = function(destination) {
   var scale = this.game.world.scale.x;
 
-  // show icon
-  this.tween.isRunning && this.tween.stop();
+  this.tween1.isRunning && this.tween1.stop();
+  this.tween2.isRunning && this.tween2.stop();
+
   this.position.copy(destination);
   this.scale.set(1/scale, 1/scale);
   this.renderable = true;
-  this.alpha = 1.0;
-  this.rotation = 0;
-  this.tween.start();
+  
+  this.sprite1.alpha = 1.0;
+  this.sprite1.rotation = 0;
+
+  this.sprite2.alpha = 1.0;
+  this.sprite2.rotation = 0;
+
+  this.tween1.start();
+  this.tween2.start();
 };
 
 module.exports = Indicator;
