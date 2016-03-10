0238722be88e9895dd0ac0bea91ab75f1deebdc0
Oliver Dyla <neutrino84@gmail.com>
2016-03-10 15:08:30 +0100

    added preorder section to website

--- src/website/index.js
+++ src/website/index.js
@@ -3,20 +3,50 @@ var jquery = require('jquery'),
     slick = require('slick'),
     drop = require('drop');
 
+var global = {};
+
+function resize() {
+  var width = jquery(window).width();
+  if(width <= 773) {
+    if(!global.slides) {
+      //.. create slides
+      global.slides = jquery('#prordr .description').slick({
+        accessibility: false,
+        arrows: false,
+        easing: 'easeInOutCubic',
+        slide: '.column',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '50px',
+        initialSlide: 2,
+        mobileFirst: true
+      });
+    }
+  } else if(global.slides) {
+    global.slides.slick('unslick');
+    global.slides = undefined;
+  }
+};
+
+jquery(window).on('resize', resize);
 jquery(document).ready(function() {
+  // resize
+  resize();
+
   // fullpage
   jquery('#window').fullpage({
-  	// loopBottom: true,
+    // loopBottom: true,
     animateAnchor: false,
     menu: '#navigation',
-    anchors: ['home', 'about', 'races'], //, 'preorder'],
+    anchors: ['home', 'about', 'races', 'preorder'],
     onLeave: function(index, nextIndex) {
       if(nextIndex === 1) {
         jquery('#gradient').css('opacity', '0');
-      	jquery('#logo').removeClass('show');
+        jquery('#logo').removeClass('show');
       } else {
         jquery('#gradient').css('opacity', '1');
-      	jquery('#logo').addClass('show');
+        jquery('#logo').addClass('show');
       }
     }
   });
@@ -28,13 +58,14 @@ jquery(document).ready(function() {
     autoplay: true,
     autoplaySpeed: 1000,
     easing: 'easeInOutCubic',
-   	slide: '.ship',
+    slide: '.ship',
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
     arrows: false,
     infinite: true,
-    fade: true
+    fade: true,
+    mobileFirst: true
   });
 
   // mobilemenu
