4d5c5fae6f82c99106db8870e9dc6aa6f50c6984
Oliver Dyla <neutrino84@gmail.com>
2016-03-12 19:27:05 +0100

    added db rebuild functionality

--- db.js
+++ db.js
@@ -2,6 +2,7 @@
 var path = require('path'),
     nconf = require('nconf'),
     redis = require('redis'),
+    async = require('async'),
     Model = require('./src/model'),
     Database = require('./src/database'),
     debugArgIdx, debugArgs = ['--debug', '--debug-brk'];
@@ -34,7 +35,48 @@ function startDatabaseConnection() {
   this.schema.adapter.initialize(client);
 
   global.model = new Model({});
+
+  this.schema.on('connected', function() {
+    var done = function(err) { console.log('err', err); },
+        create = function(user) {
+          var user = new global.model.User({
+            name: user.username,
+            username: user.username,
+            email: user.email,
+            password: user.password,
+            created: user.created,
+            role: user.role
+          });
+          user.sanitize();
+          return user;
+        },
+        worker = function(user, callback) {
+          console.log('queue', user.username);
+          user.save(callback);
+        },
+        q = async.queue(worker, 1);
+    for(var u in userlist) {
+      q.push(create(userlist[u]), done);
+    }
+    q.drain = function() {
+      console.log('work complete');
+    }
+  });
+
+  // global.model.User.find({}, function(err, users) {
+  //   for(var u in users) {
+  //     users[u].destroy(function(err) {
+  //       if(!err) {
+  //         console.log('destroyed');
+  //       } else {
+  //         console.log('error');
+  //       }
+  //     });
+  //   }
+  // });
 };
+
+var userlist = [];
 
 // init db
 startDatabaseConnection();
