5c424b979cf2bdb34b21d355bcf75af383bd1018
Oliver Dyla <neutrino84@gmail.com>
2016-03-23 13:37:39 +0100

    add cap sprite to ship pane hardpoint fitting interface

--- src/client/ui/panes/Ship/ShipPane.js
+++ src/client/ui/panes/Ship/ShipPane.js
@@ -12,6 +12,7 @@ function ShipPane(game) {
   this.data = null;
   this.drag = null;
   this.hardpoints = {};
+  this.caps = {};
 
   this.setPreferredSize(256, 228);
 };
@@ -20,8 +21,7 @@ ShipPane.prototype = Object.create(Panel.prototype);
 ShipPane.prototype.constructor = ShipPane;
 
 ShipPane.prototype.create = function(data) {
-  var game = this.game,
-      hardpoint;
+  var game = this.game;
 
   // set data
   this.data = data;
@@ -46,10 +46,13 @@ ShipPane.prototype.create = function(data) {
 
   // create hardpoint slots
   for(var h in this.config) {
-    hardpoint = this._hardpoint(data.hardpoints[h], this.config[h]);
-    hardpoint.id = h;
-    this.hardpoints[h] = hardpoint;
-    this.chassis.addChild(hardpoint);
+    this.hardpoints[h] = this._hardpoint(data.hardpoints[h], this.config[h]);
+    this.hardpoints[h].id = h;
+    
+    this.caps[h] = this._cap(this.config[h]);
+
+    this.chassis.addChild(this.hardpoints[h]);
+    this.chassis.addChild(this.caps[h]);
   }
 };
 
@@ -97,6 +100,13 @@ ShipPane.prototype._hardpoint = function(data, config) {
   return hardpoint;
 };
 
+ShipPane.prototype._cap = function(config) {
+  var cap = new engine.Sprite(game, 'texture-atlas', 'turret-cap-ubaidian.png');
+      cap.position.set(config.position.x, config.position.y);
+      cap.pivot.set(config.pivot.x, config.pivot.y);
+  return cap;
+}
+
 ShipPane.prototype._hardpointInputDown = function(sprite, pointer) {
   var game = this.game,
       // hardpoints = this.data.hardpoints,
@@ -104,7 +114,7 @@ ShipPane.prototype._hardpointInputDown = function(sprite, pointer) {
       position = sprite.worldTransform.apply(sprite.pivot),
       highlight;
   if(data != undefined) {
-    sprite.alpha = 0.25;
+    sprite.alpha = 0.5;
 
     highlight = new engine.Sprite(game, sprite.texture);
     highlight.tint = 0x5599FF;
@@ -138,7 +148,7 @@ ShipPane.prototype._hardpointInputUp = function(sprite, pointer) {
     this.drag.destroy();
     delete this.hardpoints[sprite.id];
   } else {
-    tween.to({ x: dragStartPoint.x, y: dragStartPoint.y }, 150, engine.Easing.Quadratic.Out, true);
+    tween.to({ x: dragStartPoint.x, y: dragStartPoint.y }, 100, engine.Easing.Quadratic.Out, true);
     tween.on('complete', function() {
       sprite.alpha = 1.0;
       this.drag.destroy();
@@ -147,14 +157,25 @@ ShipPane.prototype._hardpointInputUp = function(sprite, pointer) {
 };
 
 ShipPane.prototype._hardpointInputOver = function(sprite, pointer) {
-  var highlight = new engine.Sprite(game, sprite.texture);
+  var game = this.game,
+      cap = this.caps[sprite.id],
+      highlight = new engine.Sprite(game, sprite.texture);
       highlight.tint = 0x5599FF;
       highlight.blendMode = engine.BlendMode.ADD;
+
+  cap.tween && cap.tween.stop();
+  cap.tween = game.tweens.create(cap);
+  cap.tween.to({ x: sprite.x + 5 }, 100, engine.Easing.Quadratic.InOut, true);
 
   sprite.addChild(highlight);
 };
 
 ShipPane.prototype._hardpointInputOut = function(sprite, pointer) {
+  var cap = this.caps[sprite.id];
+      
+  cap.tween && cap.tween.stop();
+  cap.tween.to({ x: sprite.x }, 100, engine.Easing.Default, true);
+  
   this.timeout && clearTimeout(this.timeout);
   this.timeout = setTimeout(function() {
     sprite.removeChildren();
