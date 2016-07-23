fcaa4044bff72e0e006c5f55982cde7daedff9fc
Oliver Dyla <neutrino84@gmail.com>
2016-07-23 11:24:20 +0200

    cleanup

--- src/core/Timeout.js
+++ src/core/Timeout.js
@@ -3,12 +3,12 @@ function Timeout(game) {
   this.game = game;
   this.isRunning = false;
 
-  this._startTime = 0;
-  this._endTime = 0;
+  // this._startTime = 0;
+  // this._endTime = 0;
 
   this._onLoop = null;
   this._onUpdate = null;
-  this._timeoutID = null;
+  this._timeout = null;
 };
 
 Timeout.prototype.constructor = Timeout;
@@ -19,24 +19,25 @@ Timeout.prototype.init = function() {
 
 Timeout.prototype.start = function() {
   var self = this;
+  // this._startTime = global.Date.now();
   this._onLoop = function() {
     return self.update();
   };
-  this._timeoutID = global.setTimeout(this._onLoop, 0);
+  this._timeout = global.setTimeout(this._onLoop, 0);
 };
 
 Timeout.prototype.stop = function() {
   this.isRunning = false;
-  global.clearTimeout(this._timeoutID);
+  global.clearTimeout(this._timeout);
 };
 
 Timeout.prototype.update = function() {
-  this._endTime = global.Date.now();
+  // this._endTime = global.Date.now();
 
-  this.game.update(this._endTime - this._startTime);
-
-  this._timeoutID = global.setTimeout(this._onLoop, 0);
-  this._startTime = global.Date.now();
+  this.game.update(); //(this._endTime - this._startTime);
+  
+  this._timeout = global.setTimeout(this._onLoop, 0);
+  // this._startTime = global.Date.now();
 };
 
 module.exports = Timeout;
