9f40768acda47ab36ca94718253bda4df20c091e
Oliver Dyla <neutrino84@gmail.com>
2016-07-23 11:49:39 +0200

    updated client side movement and selection

--- src/client/objects/sector/Movement.js
+++ src/client/objects/sector/Movement.js
@@ -1,314 +1,125 @@
 
-var engine = require('engine'),
-    Animation = require('./Animation'),
-    ArcPath = require('./interpolation/ArcPath'),
-    LinearPath = require('./interpolation/LinearPath'),
-    EventEmitter = require('eventemitter3');
+var engine = require('engine');
 
 function Movement(parent) {
   this.parent = parent;
 
   this.game = parent.game;
   this.config = parent.config;
-  this.throttle = parent.throttle;
+  this.velocity = 0;
+  
+  this._last = 0;
+  this._delay = 0;
+  this._speed = 0;
 
-  this.valid = true;
-  this.anticlockwise = false;
+  this._destination = new engine.Point();
+  this._position = new engine.Point();
+  this._vector = new engine.Point();
+  this._direction = new engine.Point();
+  this._temp = new engine.Point();
 
-  this.destination = new engine.Point();
-  this.animation = new Animation(parent.game, parent);
+  this._move = this.game.clock.throttle(this.move, 100, this);
+  this._test = this.game.clock.throttle(function() {
 
-  this._tempPoint = new engine.Point();
-
-  EventEmitter.call(this);
+  }, 2000, this);
 }
 
-Movement.FRAMERATE = 60;
-Movement.SPEED_CONSTANT = 30.0;
-
-Movement.prototype = Object.create(EventEmitter.prototype);
 Movement.prototype.constructor = Movement;
 
-Movement.prototype.reset = function() {
-  this._linearSpeed = null;
-  this._arcSpeed = null;
-};
+// Movement.prototype.move = function() {
+//   var ship = this.parent,
+//       vector = this._vector,
+//       temp = this._temp,
+//       manager = ship.manager,
+//       start = manager.shipsGroup.worldTransform.apply(ship.position, temp),
+//       end = this.game.input.mousePointer,
+//       destination = { x: end.x - start.x, y: end.y - start.y };
+
+//   // normalize destination
+//   temp.copyFrom(destination).normalize();
+
+//   // plot vector to server
+//   if((engine.Math.floorTo(vector.x, 2) !== engine.Math.floorTo(temp.x, 2)) || (
+//       engine.Math.floorTo(vector.y, 2) !== engine.Math.floorTo(temp.y, 2))) {
+//     manager.socket.emit('ship/plot', {
+//       uuid: ship.uuid,
+//       destination: destination
+//     });
+//   }
+// };
 
 Movement.prototype.update = function() {
-  var parent = this.parent,
-      animation = this.animation,
-      lastFrame = this.lastFrame = parent.position.clone(this._tempPoint),
-      a1, a2;
+  var position = this._position,
+      destination = this._destination,
+      vector = this._vector,
+      direction = this._direction,
+      spd = this._speed,
+      ship = this.parent,
+      distance, speed, multiplier;
 
-  this.animation.update();
+  // move ship
+  // if(ship.isPlayer) {
+  //   this._move();
+  // }
 
-  if(animation.isPlaying && lastFrame) {
-    a1 = lastFrame.y - parent.position.y;
-    a2 = lastFrame.x - parent.position.x;
+  // ship position to point
+  position.set(ship.position.x, ship.position.y);
+
+  // calculate distance
+  distance = this.velocity = position.distance(destination);
+
+  // calculate speed
+  speed = (spd / (1/10)) * (1/60);
+  multiplier = speed / distance < 1.0 ? speed * 1.001 : speed * 0.999;
+  
+  // calculate vector
+  vector.set(destination.x - position.x, destination.y - position.y);
+  vector.normalize();
+
+  // update direction
+  direction.interpolate({
+    x: vector.x * multiplier,
+    y: vector.y * multiplier }, 0.15, direction);
+
+  // update ship position
+  ship.position.set(position.x + direction.x, position.y + direction.y);
+
+  // update rotation
+  if(spd > 0) {
+    a1 = position.y - ship.position.y;
+    a2 = position.x - ship.position.x;
     if(a1 !== 0 && a2 !== 0) {
-      parent.rotation = global.Math.atan2(a1, a2);
+      ship.rotation = global.Math.atan2(a1, a2);
     } else {
-      parent.rotation = 0;
+      ship.rotation = 0;
     }
   }
+
+  // if(destination.x !== position.x) {
+  //   this._test();
+  // }
 };
 
-Movement.prototype.plot = function(destination, current, previous, delay) {
-  var start = current ? current : this.current,
-      end = previous ? previous : this.previous,
-      destination = destination || this.destination,
-      oribital = this._findTangentCircle(start, end, destination);
+Movement.prototype.plot = function(data) {
+  var time = this.game.clock.time,
+      a1, a2, ship = this.parent,
+      distance;
 
-  if(oribital) {
-    this.valid = true;
+  this._delay = time - this._last;
+  this._last = time;
+  this._destination.copyFrom(data.pos);
+  this._speed = data.spd;
 
-    this.oribital = oribital;
-
-    this.currentPosition = start;
-    this.previousPosition = end;
-    this.destination.copyFrom(destination);
-
-    this.tangentPoint = this._findTangentPoint(this.destination, this.oribital);
-    this.startAngle = this.oribital.circumferenceAngle(this.currentPosition);
-    this.endAngle = this.oribital.circumferenceAngle(this.tangentPoint);
-
-    this.arcLength = this._findArcLength(this.currentPosition, this.tangentPoint, this.oribital);
-    this.linearLength = this.tangentPoint.distance(this.destination);
-
-    this.animation.stop(true);
-    this.animation.play(delay || 0, [
-      new ArcPath(this, 0.0, 1.0),
-      new LinearPath(this, 0.0, 0.5),
-      new LinearPath(this, 0.5, 1.0, engine.Easing.Quadratic.Out)
-    ]);
-  } else {
-    this.valid = false;
+  // update position
+  if(this._position.distance(this._destination) > 256) {
+    ship.position.set(this._destination.x, this._destination.y);
   }
 };
 
 Movement.prototype.destroy = function() {
-  // this.animation.stop();
-  this.animation.destroy();
-  this.removeAllListeners();
-  this.parent = this.game =
-    this.config = this.destination =
-    this.animation = this.currentPosition =
-    this.previousPosition = this.oribital =
-    this.tangentPoint = undefined;
+  this.parent = this.game = this.config = this._target =
+    this._destination = this._origin = this._position =
+    this._vector = this._direction = this._temp = undefined;
 };
-
-Movement.prototype.drawDebug = function() {
-  if(!this.parent.isPlayer || !this.parent.manager.trajectoryGraphics) { return; }
-
-  // draw line from start to destination
-  // this.parent.manager.trajectoryGraphics.lineStyle(1.0, 0x6699FF, 1.0);
-  // this.parent.manager.trajectoryGraphics.moveTo(this.currentPosition.x, this.currentPosition.y);
-  // this.parent.manager.trajectoryGraphics.lineTo(this.previousPosition.x, this.previousPosition.y);
-
-  // draw start circle
-  // this.parent.manager.trajectoryGraphics.lineStyle(1.0, 0xFFFFFF, 0.25);
-  // this.parent.manager.trajectoryGraphics.beginFill(0x000000, 1.0)
-  // this.parent.manager.trajectoryGraphics.drawCircle(this.currentPosition.x, this.currentPosition.y, 5);
-  // this.parent.manager.trajectoryGraphics.endFill();
-
-  // draw destination circle
-  this.parent.manager.trajectoryGraphics.lineStyle(0);
-  this.parent.manager.trajectoryGraphics.beginFill(0x00FFFF, 0.5);
-  this.parent.manager.trajectoryGraphics.drawCircle(this.destination.x, this.destination.y, 10);
-  this.parent.manager.trajectoryGraphics.endFill();
-
-  // draw turning oribit
-  // this.parent.manager.trajectoryGraphics.lineStyle(1.0, 0xFFFFFF, 0.1);
-  // this.parent.manager.trajectoryGraphics.drawCircle(this.oribital.x, this.oribital.y, this.oribital.radius);
-  this.parent.manager.trajectoryGraphics.lineStyle(5.0, 0x00FFFF, 0.5);
-  this.parent.manager.trajectoryGraphics.arc(this.oribital.x, this.oribital.y, this.oribital.radius, this.startAngle, this.endAngle, this.anticlockwise);
-  this.parent.manager.trajectoryGraphics.moveTo(this.tangentPoint.x, this.tangentPoint.y);
-  this.parent.manager.trajectoryGraphics.lineTo(this.destination.x, this.destination.y);
-
-  // draw tangent lines to destination
-  // this.parent.manager.trajectoryGraphics.lineStyle(1.0, 0xFFFFFF, 0.5);
-  // this.parent.manager.trajectoryGraphics.moveTo(this.destination.x, this.destination.y);
-  // this.parent.manager.trajectoryGraphics.lineTo(this.tangentPoint.x, this.tangentPoint.y);
-};
-
-Movement.prototype._findTangentCircle = function(start, end, destination) {
-  var radius = this.config.oribit.radius,
-      p = new engine.Point(),
-      perp = p.copyFrom(end).rotate(start.x, start.y, -global.Math.PI / 2),
-      point1 = engine.Line.pointAtDistance(start, perp, radius),
-      point2 = engine.Line.pointAtDistance(start, perp, -radius),
-      len1 = point1.distance(destination),
-      len2 = point2.distance(destination),
-      circle1 = new engine.Circle(point1.x, point1.y, radius)
-      circle2 = new engine.Circle(point2.x, point2.y, radius);
-  if(len1 > len2 && !circle2.contains(destination.x, destination.y)) {
-    this.anticlockwise = true;
-    return circle2;
-  }
-  if(len1 < len2 && !circle1.contains(destination.x, destination.y)) {
-    this.anticlockwise = false;
-    return circle1;
-  }
-  return false;
-};
-
-Movement.prototype._findTangentPoint = function(point, circle) {
-  // find tangents
-  var t, p = new engine.Point(),
-      dx = circle.x - point.x,
-      dy = circle.y - point.y,
-      dd = global.Math.sqrt(dx * dx + dy * dy),
-      a = global.Math.asin(circle.radius / dd),
-      b = global.Math.atan2(dy, dx);
-  if(this.anticlockwise) {
-    t = b - a;
-    p.setTo(
-      circle.x + circle.radius * global.Math.sin(t),
-      circle.y + circle.radius * -global.Math.cos(t)
-    );
-  } else {
-    t = b + a;
-    p.setTo(
-      circle.x + circle.radius * -global.Math.sin(t),
-      circle.y + circle.radius * global.Math.cos(t)
-    );
-  }
-  return p;
-};
-
-Movement.prototype._findArcLength = function(a, b, circle) { // function(a1, a2, circle) {
-  var a1 = a.y - circle.y,
-      a2 = a.x - circle.x,
-      b1 = b.y - circle.y,
-      b2 = b.x - circle.x,
-      angle = global.Math.atan2(a1, a2) - global.Math.atan2(b1, b2);
-  if(angle < 0) {
-    angle += 2 * global.Math.PI;
-  }
-  if(!this.anticlockwise) {
-    angle = 2 * global.Math.PI - angle;
-  }
-  return angle * circle.radius;
-};
-
-Movement.prototype._generatePreviousPosition = function() {
-  var rotation = this.parent.rotation,
-      start = this.current;
-  return new engine.Point(
-    start.x + (1 * global.Math.cos(rotation)),
-    start.y + (1 * global.Math.sin(rotation)));
-};
-
-Object.defineProperty(Movement.prototype, 'current', {
-  get: function() {
-    var current,
-        currentFrame = this.animation.currentFrame;
-    if(!currentFrame) {
-      current = this.parent.position.clone();
-      currentFrame = { x: current.x, y: current.y };
-    }
-    return currentFrame;
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'previous', {
-  get: function() {
-    var previous,
-        previousFrame = this.animation.lastFrame;
-    if(!previousFrame) {
-      previous = this._generatePreviousPosition();
-      previousFrame = { x: previous.x, y: previous.y };
-    }
-    return previousFrame;
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'throttle', {
-  get: function() {
-    return this._throttle;
-  },
-
-  set: function(value) {
-    this._linearSpeed = null;
-    this._arcSpeed = null;
-    this._throttle = value;
-    this._linearSpeed = this.linearSpeed;
-    this._arcSpeed = this.arcSpeed;
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'linearSpeed', {
-  get: function() {
-    return this._linearSpeed || (this._throttle * (Movement.SPEED_CONSTANT / this.parent.speed));
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'arcSpeed', {
-  get: function() {
-    return this._arcSpeed || (this.config.oribit.radius / this.linearSpeed);
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'maxSpeed', {
-  get: function() {
-    if(this._maxSpeed) {
-      return this._maxSpeed;
-    }
-    return this._maxSpeed = 1 / (Movement.SPEED_CONSTANT / this.config.stats.speed);
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'speed', {
-  get: function() {
-    var frame, x1, x2, y1, y2,
-        speed = 0,
-        parent = this.parent,
-        animation = this.animation,
-        lastFrame = this.lastFrame;
-    if(lastFrame && this.animation.isPlaying) {
-      frame = parent.position;
-      x1 = lastFrame.x;
-      y1 = lastFrame.y;
-      x2 = frame.x;
-      y2 = frame.y;
-      speed = global.Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-      speed /= (1 / Movement.FRAMERATE) * 1000;
-    }
-    return speed;
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'vector', {
-  get: function() {
-    var parent = this.parent,
-        animation = this.animation,
-        lastFrame = this.lastFrame,
-        x1 = y1 = x2 = y2 = 0;
-    if(lastFrame) {
-      frame = parent.position;
-      x1 = lastFrame.x;
-      y1 = lastFrame.y;
-      x2 = frame.x;
-      y2 = frame.y;
-    }
-    return { x: x2 - x1, y: y2 - y1 };
-  }
-});
-
-Object.defineProperty(Movement.prototype, 'angle', {
-  get: function() {
-    var animation = this.animation,
-        lastFrame = animation.lastFrame,
-        x1 = y1 = x2 = y2 = 0;
-    if(lastFrame) {
-      frame = animation.frame;
-      x1 = lastFrame.x;
-      y1 = lastFrame.y;
-      x2 = frame.x;
-      y2 = frame.y;
-    }
-    return global.Math.atan2(y1 - y2, x1 - x2);
-  }
-});
 
 module.exports = Movement;
--- src/client/objects/sector/Selection.js
+++ src/client/objects/sector/Selection.js
@@ -1,153 +1,37 @@
 
-var engine = require('engine'),
-    Indicator = require('./misc/Indicator');
+var engine = require('engine');
 
 function Selection(manager) {
   this.manager = manager;
   this.game = manager.game;
-  this.socket = this.game.net.socket;
-  
-  this.pixelPerfectAlpha = 1.0;
-  this.tempPoint = new engine.Point();
-  this.shipsGroup = manager.shipManager.shipsGroup;
+  this.input = game.input,
 
-  // icon
-  this.indicator = new Indicator(this.game);
-  this.game.world.add(this.indicator);
-
-  // subscribe to messages
-  this.game.on('gui/selected', this._selected, this);
+  this.input.on('onDown', this._onInput, this);
+  this.input.on('onUp', this._onInput, this);
 };
 
 Selection.prototype.constructor = Selection;
 
-Selection.prototype.destroy = function() {
-  this.game.removeListener('gui/selected', this._selected);
-
-  this.manager = this.game =
-    this.socket = this.shipsGroup = undefined;
-};
-
-Selection.prototype._plot = function(ship, destination) {
-  // show icon
-  this.indicator.show(destination);
-  
-  // plot server
-  this.socket.emit('ship/plot', {
-    uuid: ship.uuid,
-    destination: destination,
-    current: ship.movement.current,
-    previous: ship.movement.previous,
-    rotation: ship.rotation
-  });
-
-  // plot local
-  ship.movement.plot(destination);
-};
-
-Selection.prototype._selected = function(pointer, rectangle) {
-  var target, highest,
-      highestRenderOrderID = 0,
-      destination, ship,
-      select = [],
-      player = [],
-      targets = [],
-      selected = [];
-
-  this.shipsGroup.forEach(function(child) {
-    if(pointer.button === engine.Mouse.LEFT_BUTTON) {
-      if(child.overlap(rectangle)) {
-        select.push(child);
-      }
-      if(child.isPlayer) {
-        player.push(child);
-
-        if(child.target) {
-          targets.push(child.target);
+Selection.prototype._onInput = function(pointer) {
+  var data = {
+        type: pointer.isDown ? 'start' : 'stop',
+        target: {
+          x: pointer.x,
+          y: pointer.y
         }
-      }
-    }
-    if(pointer.button === engine.Mouse.RIGHT_BUTTON) {
-      if(child.selected) {
-        selected.push(child);
-      }
-    }
-  });
-
-  // always update
+      };
   if(pointer.button === engine.Mouse.LEFT_BUTTON) {
-    if(rectangle.volume <= 300) {
-      for(var s in select) {
-        if(this._checkPixel(select[s].chassis, pointer) &&
-            select[s].z > highestRenderOrderID) {
-          highest = select[s];
-          highestRenderOrderID = select[s].z;
-        }
-      }
-      select = highest ? [highest].concat(targets) : select.concat(targets);
-    }
-    this.game.emit('ships/selected', select.concat(player));
-  }
-
-  if(selected.length > 0) {
-    destination = this.shipsGroup.worldTransform.applyInverse(pointer);
-    
-    for(var i=0; i<selected.length; i++) {
-      ship = selected[i];
-      if(ship.isPlayer && !ship.destroyed) {
-        this._plot(ship, destination);
-      }
-    }
+    this.game.emit('ship/primary', data);
+  } else if(pointer.button === engine.Mouse.RIGHT_BUTTON) {
+    this.game.emit('ship/secondary', data);
   }
 };
 
-Selection.prototype._checkPixel = function(sprite, pointer) {
-  var x, y,
-      input = this.game.input,
-      texture = sprite.texture;
-  if(texture.baseTexture.source) {
-    input.getLocalPosition(sprite, pointer, this.tempPoint);
-    x = this.tempPoint.x;
-    y = this.tempPoint.y;
-
-    if(sprite.anchor.x !== 0) {
-      x -= -texture.frame.width * sprite.anchor.x;
-    }
-
-    if(sprite.anchor.y !== 0) {
-      y -= -texture.frame.height * sprite.anchor.y;
-    }
-
-    x += texture.frame.x;
-    y += texture.frame.y;
-
-    if(texture.trim) {
-      x -= texture.trim.x;
-      y -= texture.trim.y;
-
-      // if the coordinates are outside the trim area we return
-      // false immediately, to save doing a draw call
-      if(x < texture.orig.x || x > texture.orig.right ||
-          y < texture.orig.y || y > texture.orig.bottom) {
-        this._dx = x;
-        this._dy = y;
-        return false;
-      }
-    }
-
-    this._dx = x;
-    this._dy = y;
-
-    input.hitContext.clearRect(0, 0, 1, 1);
-    input.hitContext.drawImage(texture.baseTexture.source, x, y, 1, 1, 0, 0, 1, 1);
-
-    var rgb = input.hitContext.getImageData(0, 0, 1, 1);
-    if(rgb.data[3] >= this.pixelPerfectAlpha) {
-      return true;
-    }
-  }
-
-  return false;
+Selection.prototype.destroy = function() {
+  this.input.removeListener('onDown', this._onInput);
+  this.input.removeListener('onUp', this._onInput);
+  this.manager = this.game =
+    this.input = undefined;
 };
 
 module.exports = Selection;
