f07f977c98e95882cbe8e63ceca0d56c754ab93a
Oliver Dyla <neutrino84@gmail.com>
2016-08-08 21:20:26 +0200

    added more pirates

--- src/core/ShipManager.js
+++ src/core/ShipManager.js
@@ -215,10 +215,10 @@ ShipManager.prototype.generateRandomShips = function() {
   var iterator = {
         'ubaidian-x01': { race: 'ubaidian', count: 0 },
         'ubaidian-x02': { race: 'ubaidian', count: 0 },
-        'ubaidian-x03': { race: 'ubaidian', count: 2 },
-        'ubaidian-x04': { race: 'ubaidian', count: 4 },
+        'ubaidian-x03': { race: 'ubaidian', count: 0 },
+        'ubaidian-x04': { race: 'ubaidian', count: 0 },
         'hederaa-x01': { race: 'hederaa', count: 0 },
-        'mechan-x01': { race: 'mechan', count: 4 },
+        'mechan-x01': { race: 'mechan', count: 0 },
         'general-x01': { race: 'ubaidian', count: 0 },
         'general-x02': { race: 'ubaidian', count: 0 }
       };
@@ -235,16 +235,12 @@ ShipManager.prototype.generatePirateShips = function() {
         { name: 'mavero', chassis: 'general-x01' },
         { name: 'vega', chassis: 'general-x02' },
         { name: 'thak', chassis: 'general-x02' },
-        { name: 'bongo', chassis: 'general-x03'},
         { name: 'pizza', chassis: 'general-x01' },
         { name: 'cheeseman', chassis: 'general-x01' },
         { name: 'pickle', chassis: 'general-x02' },
-        { name: 'orange', chassis: 'general-x02' },
-        { name: 'muffin', chassis: 'general-x03'},
         { name: 'judo', chassis: 'general-x01' },
         { name: 'gibblie', chassis: 'general-x02' },
         { name: 'orange', chassis: 'general-x02' },
-        { name: 'honeybadger', chassis: 'general-x03'},
         { name: 'zeus', chassis: 'ubaidian-x02'}
       ],
       len = iterator.length;
