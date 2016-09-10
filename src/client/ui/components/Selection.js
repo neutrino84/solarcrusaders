e3639fd7c8d9bcd9443b1595ab1dcbacf773f0e7
Oliver Dyla <neutrino84@gmail.com>
2016-09-10 13:12:53 -0700

    remove tap listener

--- src/client/ui/components/Selection.js
+++ src/client/ui/components/Selection.js
@@ -27,21 +27,21 @@ Selection.prototype._selected = function(pointer, rectangle) {
   this.game.emit('gui/selected', pointer, rectangle);
 };
 
-Selection.prototype._tap = function(pointer, doubleTap) {
-  if(doubleTap) {
-    pointer.button = engine.Mouse.RIGHT_BUTTON;
-    // this.game.emit('gui/selected', pointer, -- rect --);
-  }
-};
+// Selection.prototype._tap = function(pointer, doubleTap) {
+//   if(doubleTap) {
+//     pointer.button = engine.Mouse.RIGHT_BUTTON;
+//     // this.game.emit('gui/selected', pointer, -- rect --);
+//   }
+// };
 
 Selection.prototype.start = function() {
   this.selection.start();
-  this.game.input.on('onTap', this._tap, this);
+  // this.game.input.on('onTap', this._tap, this);
 };
 
 Selection.prototype.stop = function() {
   this.selection.stop();
-  this.game.input.removeListener('onTap', this._tap);
+  // this.game.input.removeListener('onTap', this._tap);
 };
 
 module.exports = Selection;
