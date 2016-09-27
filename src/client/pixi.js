27eac3f94599084be84604ef82541d67650bd65c
Oliver Dyla <neutrino84@gmail.com>
2016-09-27 17:03:05 +0200

    added support for es6 pixi.js module loading

--- src/client/pixi.js
+++ src/client/pixi.js
@@ -1,33 +1,20 @@
+
+import mesh from '../libs/pixi.js/src/mesh';
+import particles from '../libs/pixi.js/src/particles';
+import extract from '../libs/pixi.js/src/extract';
+import prepare from '../libs/pixi.js/src/prepare';
+import core from '../libs/pixi.js/src/core';
+
 // run the polyfills
 require('./polyfill');
 
-var core = module.exports = require('../libs/pixi.js/src/core');
-    core.mesh = require('../libs/pixi.js/src/mesh');
-    // core.particles = require('../libs/pixi.js/src/particles');
-    core.filters = {
-      BlurFilter: require('../libs/pixi.js/src/filters/blur/BlurFilter')//,
-      // DisplacementFilter: require('../libs/pixi.js/src/filters/displacement/DisplacementFilter'),
-      // ColorMatrixFilter: require('../libs/pixi.js/src/filters/colormatrix/ColorMatrixFilter')
-    };
-    
-    // sc pixi shims
-    core.TRANSFORM_MODE.DEFAULT = 1;
+// sc pixi shims
+core.TRANSFORM_MODE.DEFAULT = 1;
 
-// add core plugins.
-// core.extras         = require('../libs/pixi.js/src/extras'); 
-// core.filters        = require('../libs/pixi.js/src/filters');
-// core.interaction    = require('../libs/pixi.js/src/interaction');
-// core.loaders        = require('../libs/pixi.js/src/loaders');
-
-// export a premade loader instance
-/**
- * A premade instance of the loader that can be used to loader resources.
- *
- * @name loader
- * @memberof PIXI
- * @property {PIXI.loaders.Loader}
- */
-// core.loader = new core.loaders.Loader();
-
-// mixin the deprecation features.
-// Object.assign(core, require('./deprecation'));
+// add core plugins
+module.exports = Object.assign(core, {
+  extract,
+  mesh,
+  particles,
+  prepare
+});
