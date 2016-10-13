b4cdab77efe7890e02862da4f03eae60f2c6bbe7
andrej <navikom11@mail.ru>
2016-10-13 19:36:21 +0300

    Added hotkeys label at the bottom of the screen

--- src/client/ui/panes/BottomPane.js
+++ src/client/ui/panes/BottomPane.js
@@ -5,6 +5,7 @@ var engine = require('engine'),
     FlowLayout = require('../layouts/FlowLayout'),
     EnhancementPane = require('./EnhancementPane'),
     VitalsPane = require('./VitalsPane'),
+    Label = require('../components/Label'),
     InventoryPane = require('./InventoryPane');
 
 function BottomPane(game) {
@@ -22,22 +23,29 @@ function BottomPane(game) {
     pane: {
       bg: false
     },
+    hotKey: {
+      bg: false,
+      text: { fontName: 'full' },
+      padding: [12, 24]
+    }
   });
 
   this.leftPane = new Pane(game, this.settings.pane);
-  // this.centerPane = new Pane(game, this.settings.pane);
+   this.centerPane = new Pane(game, this.settings.pane);
   // this.rightPane = new Pane(game, this.settings.pane);
+  var hotKeysLabel = new Label(game, "up(W), down(S), left(A), right(D)", this.settings.hotKey);
 
   this.enhancementPane = new EnhancementPane(game);
   this.vitalsPane = new VitalsPane(game);
-  // this.inventoryPane = new InventoryPane(game);
+   //this.inventoryPane = new InventoryPane(game);
 
   this.leftPane.addPanel(Layout.STRETCH, this.enhancementPane);
   this.leftPane.addPanel(Layout.NONE, this.vitalsPane);
-  // this.centerPane.addPanel(Layout.NONE, this.inventoryPane);
+  this.leftPane.addPanel(Layout.NONE, hotKeysLabel);
+   //this.centerPane.addPanel(Layout.NONE, this.inventoryPane);
 
   this.addPanel(Layout.NONE, this.leftPane);
-  // this.addPanel(Layout.NONE, this.centerPane);
+   //this.addPanel(Layout.NONE, this.centerPane);
   // this.addPanel(Layout.NONE, this.rightPane);
 };
 
