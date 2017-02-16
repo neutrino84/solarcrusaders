6c8bf6d144bc23b9830248475f1a9bbd0475ca96
Oliver Dyla <neutrino84@gmail.com>
2017-02-16 08:59:15 -0800

    removed unecessary pixi namespaces

--- src/client/pixi.js
+++ src/client/pixi.js
@@ -6,14 +6,12 @@ import './polyfill';
 import * as extras from '../libs/pixi.js/src/extras';
 import * as mesh from '../libs/pixi.js/src/mesh';
 import * as particles from '../libs/pixi.js/src/particles';
-import * as extract from '../libs/pixi.js/src/extract';
-import * as prepare from '../libs/pixi.js/src/prepare';
+import * as filters from '../libs/pixi.js/src/filters';
 
 export * from '../libs/pixi.js/src/core';
 export {
   extras,
-  extract,
   mesh,
-  particles,
-  prepare
+  filters,
+  particles
 };
