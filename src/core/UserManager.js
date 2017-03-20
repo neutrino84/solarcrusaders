f17e5fedb0fab0cea8922562baeba6cbb40c65b4
Oliver Dyla <neutrino84@gmail.com>
2017-03-19 21:28:57 -0700

    additional updates to experimental socket upgrade

--- src/core/UserManager.js
+++ src/core/UserManager.js
@@ -5,55 +5,56 @@ var winston = require('winston'),
 function UserManager(game) {
   this.game = game;
   this.model = game.model;
+  this.sockets = game.sockets.ioserver;
 
   this.users = {};
 
   this.game.on('auth/connect', this.connect, this);
   this.game.on('auth/disconnect', this.disconnect, this);
-  this.game.on('auth/login', this.login, this);
   this.game.on('auth/data', this.data, this);
+  this.game.on('auth/remove', this.remove, this); 
 };
 
 UserManager.prototype.constructor = UserManager;
 
-UserManager.prototype.init = function() {};
+UserManager.prototype.init = function() {
+
+};
+
+UserManager.prototype.add = function(user) {
+  this.users[user.uuid] = user;
+};
+
+UserManager.prototype.remove = function(user) {
+  delete this.users[user.uuid];
+};
 
 UserManager.prototype.connect = function(socket) {
   var session = socket.request.session,
-      user = session.user;
+      user = session ? session.user : false;
   if(user) {
-    if(this.users[user.uuid]) {
-      // accessing current user
+    if(this.exists(user)) {
       winston.info('[UserManager] User already exists in game');
-      this.users[user.uuid].socket = socket;
-      this.game.emit('auth/login', this.users[user.uuid]);
-    } else if(socket) {
-      // create a new user
-      winston.info('[UserManager] Creating new user in game');
-      user = this.users[user.uuid] = new User(this, user, socket);
-      user.init(function(err) {
-        this.game.emit('auth/login', user);
+    } else if(user && socket && session) {
+      winston.info('[UserManager] Creating user in game');
+      user = new User(this, user, socket);
+      user.init(function(err, data) {
+        this.add(user);
       }, this);
+    } else {
+      winston.info('[UserManager] User session data error');
+      socket.disconnect(true);
     }
   } else {
-    winston.info('[UserManager] User session data was never created');
-    socket.disconnect();
+    winston.info('[UserManager] User session data error');
+    socket.disconnect(true);
   }
 };
 
 UserManager.prototype.disconnect = function(socket) {
-  var game = this.game,
-      session = socket.request.session,
-      user = this.users[session.user.uuid];
-      user && user.destroy();
-};
-
-UserManager.prototype.login = function(user) {
-  user.socket.emit('auth/sync', user.toStreamObject());
-};
-
-UserManager.prototype.logout = function(user) {
-  user.socket.disconnect();
+  var session = socket.request.session,
+      user = session.user;
+      session && user && user.destroy();
 };
 
 UserManager.prototype.data = function() {
@@ -62,6 +63,10 @@ UserManager.prototype.data = function() {
 
 UserManager.prototype.update = function() {
   //..
+};
+
+UserManager.prototype.exists = function(user) {
+  return user && this.users[user.uuid] ? true : false;
 };
 
 module.exports = UserManager;
