fd463d0549243c9b06f7d81fa300a3be674c1c0e
Oliver Dyla <neutrino84@gmail.com>
2017-07-24 14:04:38 -0700

    updated sector management and logic

--- src/core/SectorManager.js
+++ src/core/SectorManager.js
@@ -4,27 +4,103 @@ var uuid = require('uuid'),
     client = require('client'),
     ShipManager = require('./ShipManager'),
     UserManager = require('./UserManager'),
-    StationManager = require('./StationManager');
+    StationManager = require('./StationManager'),
+    PlayManager = require('./PlayManager');
 
 function SectorManager(game) {
   this.game = game;
-  this.userManager = new UserManager(game);
-  this.shipManager = new ShipManager(game);
+  this.sockets = game.sockets;
+
+  // buffer
+  this.updates = {
+    ships: [],
+    stations: [],
+    users: []
+  };
+
+  // instance managers
   this.stationManager = new StationManager(game);
+  this.playManager = new PlayManager(game);
+  this.shipManager = new ShipManager(game);
+  this.userManager = new UserManager(game);
 };
 
 SectorManager.prototype.constructor = SectorManager;
 
 SectorManager.prototype.init = function() {
-  this.userManager.init();
-  this.shipManager.init();
   this.stationManager.init();
+  this.playManager.init();
+  this.shipManager.init();
+  this.userManager.init();
+
+  // user request data
+  this.sockets.on('sector/data', this.data, this);
+
+  // listen for data
+  this.game.on('ship/data', this.queue('ships'), this);
+  this.game.on('station/data', this.queue('stations'), this);
+  this.game.on('user/data', this.queue('users'), this);
+
+  // queue
+  this.game.clock.events.loop(50, this.queued, this);
 };
 
 SectorManager.prototype.update = function() {
-  this.userManager.update();
-  this.shipManager.update();
-  this.stationManager.update();
+  var sockets = this.sockets,
+      users = this.userManager.all(),
+      ships = this.shipManager.sync(),
+      stations = this.stationManager.sync();
+
+  // syncronize all
+  sockets.send('sector/sync', {
+    users: users,
+    ships: ships,
+    stations: stations
+  });
+};
+
+SectorManager.prototype.data = function(socket, args) {
+  var uuids = args[1],
+      ships = this.shipManager.data(uuids.ships),
+      stations = this.stationManager.data(uuids.stations),
+      users = this.userManager.data(uuids.users);
+
+  // relay data to user
+  socket.emit('sector/data', {
+    type: 'sync',
+    ships: ships,
+    stations: stations,
+    users: users
+  });
+};
+
+SectorManager.prototype.queue = function(key) {
+  return function(updates) {
+    this.updates[key] = this.updates[key].concat(updates);
+  }
+};
+
+SectorManager.prototype.queued = function() {
+  var updates = this.updates;
+  if(updates.ships.length > 0 ||
+      updates.stations.length > 0 ||
+      updates.users.length > 0) {
+
+    // send data
+    this.sockets.send('sector/data', {
+      type: 'update',
+      ships: updates.ships,
+      stations: updates.stations,
+      users: updates.users
+    });
+
+    // clear buffer
+    this.updates = {
+      ships: [],
+      stations: [],
+      users: []
+    };
+  }
 };
 
 module.exports = SectorManager;
