0fc26b0bcbf3d849b3928d30e6bc35dfe02489ea
Oliver Dyla <neutrino84@gmail.com>
2017-07-24 13:30:42 -0700

    removed hardpoint cooldown logic

--- src/core/objects/Hardpoint.js
+++ src/core/objects/Hardpoint.js
@@ -2,7 +2,7 @@
 var client = require('client'),
     Utils = require('../../utils');
 
-function Hardpoint(ship, slot, type, subtype) {
+function Hardpoint(ship, type, subtype, slot) {
   this.ship = ship;
   this.game = ship.game;
   this.slot = slot;
@@ -10,30 +10,9 @@ function Hardpoint(ship, slot, type, subtype) {
   this.subtype = subtype || 'basic';
 
   this.data = client.ItemConfiguration['hardpoint'][this.type][this.subtype];
-
-  this.cooling = false;
 };
 
 Hardpoint.prototype.constructor = Hardpoint;
-
-Hardpoint.prototype.cooldown = function(runtime) {
-  var rnd = this.game.rnd,
-      cooldown = this.data.cooldown + runtime,
-      time = rnd.realInRange(cooldown, cooldown*2);
-
-  this.cooling = true;
-  this.game.clock.events.add(time, this.cooled, this);
-};
-
-Hardpoint.prototype.cooled = function() {
-  this.cooling = false;
-
-  if(this.ship.user) {
-    this.ship.user.socket.emit('ship/hardpoint/cooled', {
-      slot: this.slot
-    });
-  }
-};
 
 Hardpoint.prototype.toObject = function() {
   return Utils.extend({ slot: this.slot }, this.data);
