e71681b58005870a321d3fd044ff07dd9488dafa
Oliver Dyla <neutrino84@gmail.com>
2017-10-27 22:15:26 -0700

    improved modal support and added ship selection ui

--- src/client/ui/index.js
+++ src/client/ui/index.js
@@ -10,6 +10,28 @@ var Panel = require('../ui/Panel'),
 
 function UI(game) {
   this.game = game;
+  this.auth = game.auth;
+  this.modaled = null;
+  this.settings = {
+    content: {
+      layout: {
+        type: 'border',
+        gap: [0, 0]
+      }
+    },
+    root: {
+      width: this.game.width,
+      height: this.game.height,
+      bg: {
+        color: 0x000000,
+        fillAlpha: 0.8
+      }
+    }
+  };
+
+  // subscribe to messaging
+  this.game.on('ui/modal', this.modal, this);
+  this.game.on('auth/sync', this.syncronize, this);
 };
 
 UI.prototype.preload = function() {
@@ -19,25 +41,31 @@ UI.prototype.preload = function() {
 };
 
 UI.prototype.create = function() {
-  this.shipyard = new Shipyard(this.game);
+  // create bottom enhancements
   this.bottom = new BottomPane(this.game);
+
+  // create leaderboard
   this.leaderboard = new LeaderBoardPane(this.game);
   this.leaderboard.create();
 
-  this.root = new Pane(this.game, {
-    width: this.game.width,
-    height: this.game.height,
-    layout: {
-      type: 'border',
-      gap: [0, 0]
-    },
-    bg: false
-  });
+  // create shipyard
+  this.shipyard = new Shipyard(this.game);
+  this.shipyard.create();
+
+  // create content area
+  this.content = new Pane(this.game, this.settings.content);
+
+  // create root pane
+  this.root = new Pane(this.game, this.settings.root);
+  this.root.bg.visible = false;
 
   // add elements
-  this.root.addPanel(this.bottom);
-  this.root.addPanel(this.shipyard);
-  this.root.addPanel(this.leaderboard);
+  this.content.addPanel(this.bottom);
+  this.content.addPanel(this.leaderboard);
+  this.root.addPanel(this.content);
+
+  // syncronize
+  this.syncronize();
 
   // invalidate
   this.root.invalidate();
@@ -46,8 +74,29 @@ UI.prototype.create = function() {
   this.game.stage.addChild(this.root);
 };
 
-UI.prototype.refresh = function() {
-  this.root.invalidate(true);
+UI.prototype.modal = function(pane) {
+  if(pane) {
+    this.modaled = pane;
+    this.content.visible = false;
+    this.root.bg.visible = true;
+    this.root.addPanel(this.modaled);
+    this.root.invalidate();
+  } else {
+    this.content.visible = true;
+    this.root.bg.visible = false;
+    this.root.removePanel(this.modaled);
+    this.root.invalidate();
+  }
+};
+
+UI.prototype.syncronize = function() {
+  var game = this.game,
+      auth = this.auth;
+  if(auth.user) {
+    if(!auth.user.ship) {
+      game.emit('ui/shipyard/show');
+    }
+  }
 };
 
 UI.prototype.resize = function(width, height) {
